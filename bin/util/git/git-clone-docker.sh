4d26b40649f7bbba8f41d0237f410c9a86a29df8
Attila L. Egyedi <attila.egyedi@gmail.com>
2020-10-01 13:47:26 -0700

    Docker repos

--- bin/util/git/git-clone-docker.sh
+++ bin/util/git/git-clone-docker.sh
@@ -5,10 +5,10 @@ source $CEDAR_UTIL_BIN/include-repo-list-docker.sh
 format="\n\nCloning Git repo status ${GREEN}%-32s${NORMAL} : (%-70s)\n"
 
 function cloneRepo {
-  printf "$format" $1 $CEDAR_HOME/$1
-  git -C "$CEDAR_HOME/$1" clone https://github.com/metadatacenter/$1
-  git -C "$CEDAR_HOME/$1" status
-  git -C "$CEDAR_HOME/$1" status | egrep 'Your branch is up to date with|Your branch is up-to-date with'
+  printf "$format" $1 $CEDAR_DOCKER_HOME/$1
+  git -C "$CEDAR_DOCKER_HOME/$1" clone https://github.com/metadatacenter/$1
+  git -C "$CEDAR_DOCKER_HOME/$1" status
+  git -C "$CEDAR_DOCKER_HOME/$1" status | egrep 'Your branch is up to date with|Your branch is up-to-date with'
   if [ $? == 0 ]; then
     echo "${GREEN}Up-to-date with remote :)${NORMAL}"
   else
@@ -16,7 +16,7 @@ function cloneRepo {
   fi
 }
 
-for i in "${CEDAR_REPOS[@]}"
+for i in "${CEDAR_DOCKER_REPOS[@]}"
 do
    cloneRepo $i
 done
