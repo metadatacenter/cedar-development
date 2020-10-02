30d6c74db20a20ef4ab08daaecbf798be7d4dc44
Attila L. Egyedi <attila.egyedi@gmail.com>
2020-10-02 16:26:03 -0700

    Fix path

--- bin/templates/cedar-profile-docker-eval-2.sh
+++ bin/templates/cedar-profile-docker-eval-2.sh
@@ -6,7 +6,7 @@ export CEDAR_DEVELOP_HOME=${CEDAR_DOCKER_HOME}/cedar-development
 
 #------------------------------------------------------
 # CEDAR Docker aliases
-source ${CEDAR_DOCKER_HOME}/cedar-docker-deploy/bin/util/set-env-generic.sh
+source ${CEDAR_DEVELOP_HOME}/bin/util/set-env-generic.sh
 source ${CEDAR_DOCKER_HOME}/cedar-docker-deploy/bin/util/set-docker-aliases.sh
 
 #----------------------------------------------------------
