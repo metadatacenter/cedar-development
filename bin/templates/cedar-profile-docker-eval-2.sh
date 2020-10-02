49a235d6a56d4cffaab57551ec8d01969221457e
Attila L. Egyedi <attila.egyedi@gmail.com>
2020-10-02 13:56:23 -0700

    New file

---
+++ bin/templates/cedar-profile-docker-eval-2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#------------------------------------------------------
+# CEDAR Docker Development Util home folder
+export CEDAR_DEVELOP_HOME=${CEDAR_DOCKER_HOME}/cedar-development
+
+#------------------------------------------------------
+# CEDAR Docker aliases
+source ${CEDAR_DOCKER_HOME}/cedar-docker-deploy/bin/util/set-env-generic.sh
+source ${CEDAR_DOCKER_HOME}/cedar-docker-deploy/bin/util/set-docker-aliases.sh
+
+#----------------------------------------------------------
