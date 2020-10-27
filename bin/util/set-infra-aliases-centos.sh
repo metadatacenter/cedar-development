fc63cba4123acea2c851b73b13a6d5bca405cd04
Cedar Service User <cedar@cedr-dev-app-01.stanford.edu>
2020-10-27 14:56:30 -0700

    No sudo

--- bin/util/set-infra-aliases-centos.sh
+++ bin/util/set-infra-aliases-centos.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
 #3rd party server aliases
-alias startnginx='sudo $CEDAR_UTIL_BIN/services-centos/startnginx.sh'
-alias stopnginx='sudo $CEDAR_UTIL_BIN/services-centos/stopnginx.sh'
+# We do not use sudo here, because we should not know the 
+# password for 'cedar' user.
+# We will need to 'sudo su -' to stop the infra services
+alias startnginx='$CEDAR_UTIL_BIN/services-centos/startnginx.sh'
+alias stopnginx='$CEDAR_UTIL_BIN/services-centos/stopnginx.sh'
 
-alias startmongo='sudo $CEDAR_UTIL_BIN/services-centos/startmongo.sh'
-alias stopmongo='sudo $CEDAR_UTIL_BIN/services-centos/stopmongo.sh'
+alias startmongo='$CEDAR_UTIL_BIN/services-centos/startmongo.sh'
+alias stopmongo='$CEDAR_UTIL_BIN/services-centos/stopmongo.sh'
 
-alias startmysql='sudo $CEDAR_UTIL_BIN/services-centos/startmysql.sh'
-alias stopmysql='sudo $CEDAR_UTIL_BIN/services-centos/stopmysql.sh'
+alias startmysql='$CEDAR_UTIL_BIN/services-centos/startmysql.sh'
+alias stopmysql='$CEDAR_UTIL_BIN/services-centos/stopmysql.sh'
 
-alias startkibana='sudo $CEDAR_UTIL_BIN/services-centos/startkibana.sh'
-alias stopkibana='sudo $CEDAR_UTIL_BIN/services-centos/stopkibana.sh'
+alias startkibana='$CEDAR_UTIL_BIN/services-centos/startkibana.sh'
+alias stopkibana='$CEDAR_UTIL_BIN/services-centos/stopkibana.sh'
 
-alias startelastic='sudo $CEDAR_UTIL_BIN/services-centos/startelastic.sh'
-alias stopelastic='sudo $CEDAR_UTIL_BIN/services-centos/stopelastic.sh'
+alias startelastic='$CEDAR_UTIL_BIN/services-centos/startelastic.sh'
+alias stopelastic='$CEDAR_UTIL_BIN/services-centos/stopelastic.sh'
 
-alias startneo='sudo $CEDAR_UTIL_BIN/services-centos/startneo.sh'
-alias stopneo='sudo $CEDAR_UTIL_BIN/services-centos/stopneo.sh'
+alias startneo='$CEDAR_UTIL_BIN/services-centos/startneo.sh'
+alias stopneo='$CEDAR_UTIL_BIN/services-centos/stopneo.sh'
 
-alias startredis='sudo $CEDAR_UTIL_BIN/services-centos/startredis.sh'
-alias stopredis='sudo $CEDAR_UTIL_BIN/services-centos/stopredis.sh'
+alias startredis='$CEDAR_UTIL_BIN/services-centos/startredis.sh'
+alias stopredis='$CEDAR_UTIL_BIN/services-centos/stopredis.sh'
 
-alias startrc='sudo $CEDAR_UTIL_BIN/services-centos/startrediscommander.sh'
-alias killrc='sudo $CEDAR_UTIL_BIN/services-centos/killrediscommander.sh'
+alias startrc='$CEDAR_UTIL_BIN/services-centos/startrediscommander.sh'
+alias killrc='$CEDAR_UTIL_BIN/services-centos/killrediscommander.sh'
 
 #CEDAR server aliases
 alias starteditor='goeditor && gulp'
