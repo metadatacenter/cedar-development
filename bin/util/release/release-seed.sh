de1753b6a061cda7c6b8167cdf5fcbd5c8643104
Attila L. Egyedi <attila.egyedi@gmail.com>
2020-11-23 17:57:39 -0800

    seed develop

--- bin/util/release/release-seed.sh
+++ bin/util/release/release-seed.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/metadatacenter/cedar-development/master/bin/util/release/release-all.sh
+wget https://raw.githubusercontent.com/metadatacenter/cedar-development/develop/bin/util/release/release-all.sh
