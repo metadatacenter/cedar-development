46c3821397645910b9a0b844e3040188c89d9dd5
Attila L. Egyedi <egyedia@stanford.edu>
2023-06-09 14:47:27 -0700

    Debug network for cedarat

--- bin/util/admintool/cedar-admin-tool.sh
+++ bin/util/admintool/cedar-admin-tool.sh
@@ -4,4 +4,4 @@ echo Launching CEDAR Admin Tool ${CEDAR_VERSION}
 echo ----------------------------------------------
 echo
 
-java -jar $CEDAR_HOME/cedar-admin-tool/target/cedar-admin-tool-${CEDAR_VERSION}.jar "$@"
+java -Djavax.net.debug=trustmanager -jar $CEDAR_HOME/cedar-admin-tool/target/cedar-admin-tool-${CEDAR_VERSION}.jar "$@"
