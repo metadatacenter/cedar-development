87f0e1bf9cdc4bf5f3556fb890324ba388db7721
Attila L. Egyedi <egyedia@stanford.edu>
2023-09-19 12:28:47 -0700

    Mysql Brew version change

--- bin/util/services-centos/stopopensearchdashboards.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo --------------------------------------------------------------------------------
-echo Stopping OpenSearch Dashboards
-echo --------------------------------------------------------------------------------
-echo
-
-echo "Stopping services not supported"
--- bin/util/services-osx/startneo.sh
+++ bin/util/services-osx/startneo.sh
@@ -4,5 +4,4 @@ echo Starting Neo4j
 echo --------------------------------------------------------------------------------
 echo
 
-export JAVA_HOME=$(brew --prefix)/opt/openjdk@17/
 ${CEDAR_NEO4J_HOME}/bin/neo4j start &
