cd2d8c909a9e8d9136083177f153384d15f16560
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2024-01-05 15:13:37 -0800

    Produce version of component

--- bin/util/set-env-generic.sh
+++ bin/util/set-env-generic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #CEDAR version
-export CEDAR_VERSION=2.6.49-SNAPSHOT
+export CEDAR_VERSION=2.6.50-SNAPSHOT
 
 # Keycloak connection data
 export CEDAR_KEYCLOAK_HOST="${CEDAR_NET_GATEWAY}"
